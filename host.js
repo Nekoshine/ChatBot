8b7336d62c3ca01497d96933e17537cf3ca28630
Nekoshine <uharry44@gmail.com>
2021-06-07 11:23:13 +0200

    Commentaires host client

--- host.js
+++ host.js
@@ -10,9 +10,12 @@ app.set('view engine','ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended : true}))
 
-var bot = [];
-var serverBot = [];
-var data = Array()
+var bot = []; //Tableau qui contient les bots
+var serverBot = []; //Tableau qui contient les serveurs des bots
+var data = Array(); //Tableau qui contient les donneés échangées
+/*
+Fonction qui va permettre de lancer le bot sur un serveur à part avec un port et un/des cerveau/x spécifié/s.
+*/
 function lancementServBot(port,brain){
     bot[port] = new RiveScript();
     var corsOptions = {
@@ -22,56 +25,57 @@ function lancementServBot(port,brain){
     };
     var type = typeof brain;
     console.log(type);
-    if(type == "string"){
+    if(type == "string"){ // On vérifie ici si il y'a un seul ou plusieurs cerveaux pour ce bot
         if(brain!=""){
-            bot[port].loadFile(brain).then(loading_done).catch(loading_error);
+            bot[port].loadFile(brain).then(loading_done).catch(loading_error); //On charge le cerveau
         }
     }
     else{
         for(var i=0; i<brain.length; i++){
             if(brain!=""){
-                bot[port].loadFile(brain[i]).then(loading_done).catch(loading_error);
+                bot[port].loadFile(brain[i]).then(loading_done).catch(loading_error); // On charge chaque cerveau
             }
         }
     }
-    bot[port].sortReplies();
+    run(port); //Lancement du serveur
 
+    // Lors d'une requête get sur la page du bot, on va donc pré-répondre pour accueillir l'utilisateur
     app.get('/chat', cors(corsOptions), function(req, res) {
-        var json = {"message": "Bonjour humain je suis un robot", "reponse" : ""}; 
+        data =[] //Stockage du message
+        var json = {"message": "Bonjour humain je suis un robot", "reponse" : ""}; //Message à envoyer à l'ejs
         data.push(json);
-        res.render('chat_bot',{list : data});
+        res.render('chat_bot',{list : data}); 
     });
 
-    run(port);
-
+    //Lors d'une requête post, cela signifie que l'utilisateur dialogue avec le bot il faut donc préparer la réponse du bot en fonction du cerveau 
     app.post('/chat', cors(corsOptions), function (req, res)  {
-               
+        console.log(req.headers["host"].split(':'));
+        portCerveau = req.headers["host"].split(':')[1]; //On isole le port du bot pour le retrouver dans le tableau
         const message = JSON.parse(JSON.stringify(req.body));
-        bot[port].reply("Utilisateur", message["message"]).then(function(reply) {
-            var json = {"message": message["message"], "reponse" : reply};
+        bot[portCerveau].reply("Utilisateur", message["message"]).then(function(reply) { //On construit la réponse du bot en fournissant le message envoyé par l'utilisateur
+            var json = {"message": message["message"], "reponse" : reply}; //On met en forme la réponse
             data.push(json);
-            res.render('chat_bot', { list : data});
-
+            res.render('chat_bot', { list : data}); //On envoie le tout à l'affichage
         });
     });
 
-    function loading_done() {
-      console.log("Bot has finished loading!");
+    function loading_done() { //Fonction de Rivescript permettant de signaler que le lancement du bot a réussi
+      console.log("Cerveau chargé !");
       bot[port].sortReplies();
     }
 
-    function loading_error(error, filename, lineno) {
-       console.log("Error when loading files: " + error);
+    function loading_error(error, filename, lineno) { //Fonction de Rivescript permettant de signaler que le lancement du bot a échoué
+       console.log("Erreur lors du chargement du cerveau: " + error); 
      }
 };
-    function stop(port){
-         console.log("clossing");
+    function stop(port){ //Fonction de fermeture du serveur
+         console.log("Fermeture du port :"+port);
          serverBot[port].close();
     };
 
-    function run(port){
+    function run(port){ // FOnction de lancement du serveur
         serverBot[port] = app.listen(port, function () {
-          console.log('Started listening on : '+port);
+          console.log("Port ouvert :"+port);
         });
     };
 
